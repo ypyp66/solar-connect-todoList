183f188aa1c1af745c71c08bc12c6bf2719c980c
kitebeen <57565933+ypyp66@users.noreply.github.com>
2021-08-20 00:32:38 +0900

    Initial commit

---
+++ src/components/todo/TodoService.tsx
@@ -0,0 +1,75 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useState, useEffect } from "react";
+
+export type Itodo = {
+  id: number;
+  text: string;
+  done: boolean;
+};
+
+let initialTodos: Itodo[] = [];
+
+export const useTodo = () => {
+  const [todoState, setTodoState] = useState(initialTodos);
+  var nextIdState = 0;
+
+  const incrementNextId = () => {
+    nextIdState = nextIdState + 1;
+  };
+
+  const toggleTodo = (id: number) => {
+    //@TODO
+    setTodoState((prevState) =>
+      prevState.map((todo: Itodo) =>
+        todo.id === id ? { ...todo, done: !todo.done } : todo
+      )
+    );
+  };
+
+  const removeTodo = (id: number) => {
+    setTodoState((prevState) =>
+      prevState.filter((todo: Itodo) => todo.id !== id)
+    );
+  };
+
+  const createTodo = (todo: Itodo) => {
+    const nextId = todoState.length + 1;
+    setTodoState((prevState) =>
+      prevState.concat({
+        ...todo,
+        id: nextId
+      })
+    );
+  };
+
+  const loadData = () => {
+    let data = localStorage.getItem("todos");
+    if (data === undefined) data = "";
+    initialTodos = JSON.parse(data!);
+    if (initialTodos && initialTodos.length >= 1) {
+      incrementNextId();
+    }
+    setTodoState(initialTodos);
+  };
+
+  const saveData = () => {
+    localStorage.setItem("todos", JSON.stringify(todoState));
+  };
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
+  useEffect(() => {
+    saveData();
+  }, [todoState]);
+
+  return {
+    todoState,
+    nextIdState,
+    incrementNextId,
+    toggleTodo,
+    removeTodo,
+    createTodo
+  };
+};
